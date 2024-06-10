fb47d1254a0f2ec8cca284c42e11b2d20f492fa2
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-06-10 19:48:44 +0200

    Initial commit

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { DemoRoutingModule } from './demo-routing.module';
 import { DemoComponent } from './demo.component';
 import { DemoBindingsComponent } from './demo-bindings/demo-bindings.component';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { DemoPipeComponent } from './demo-pipe/demo-pipe.component';
 
